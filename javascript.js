84ac5ffcd46777ce2f523eb4f7b44b0cc28c857e
PauliLinde <jus.paulina@gmail.com>
2025-03-26 14:09:19 +0100

    connect to API

--- javascript.js
+++ javascript.js
@@ -1,4 +1,33 @@
 
+fetch('https://fakestoreapi.com/products')
+.then(res=>res.json())
+.then(products=>render(products))
+
+function render(products){
+
+let output = '<div class="row">'
+
+products.forEach(product => {
+output += `
+<div class="col-md-4">
+<div class="card" style="width: 18rem;">
+  <img src="${product.image}" class="card-img-top" alt="${product.title}">
+  <div class="card-body">
+    <h5 class="card-title">${product.title}</h5>
+    <p class="card-text">${product.description}</p>
+    <a href="order.html?id=${product.id}" class="btn btn-primary">Buy ${product.price}</a>
+  </div>
+</div>
+</div>
+`
+});
+
+
+output += '</div>'
+
+document.getElementById('Products').innerHTML = output
+}
+  
   function validateForm() {
     let name = document.forms["customerForm"]["customerName"].value;
     if (name == "") {
@@ -30,6 +59,5 @@
       alert("City must be filled out");
       return false;
     }
-
-
   }
+    
