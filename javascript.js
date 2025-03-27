4ca920cf233b075795f2c8a3f74fdcb48b2d7b9f
PauliLinde <jus.paulina@gmail.com>
2025-03-27 13:48:40 +0100

    Connected chosen item with orderform, moved orderform's javascript to new file

--- javascript.js
+++ javascript.js
@@ -5,59 +5,29 @@ fetch('https://fakestoreapi.com/products')
 
 function render(products){
 
-let output = '<div class="row">'
+    let output = '<div class="row">'
 
-products.forEach(product => {
-output += `
-<div class="col-md-4">
-<div class="card" style="width: 18rem;">
-  <img src="${product.image}" class="card-img-top" alt="${product.title}">
-  <div class="card-body">
-    <h5 class="card-title">${product.title}</h5>
-    <p class="card-text">${product.description}</p>
-    <a href="order.html?id=${product.id}" class="btn btn-primary">Buy ${product.price}</a>
-  </div>
-</div>
-</div>
-`
-});
+    products.forEach(product => {
+      output += `
+        <div class="col-md-4">
+        <div class="card" style="width: 18rem;">
+          <img src="${product.image}" class="card-img-top" alt="${product.title}">
+          <div class="card-body">
+            <h5 class="card-title">${product.title}</h5>
+            <p class="card-text">${product.description}</p>
+            <a href="order.html?id=${product.id}" class="btn btn-primary">Buy ${product.price}$</a>
+          </div>
+        </div>
+        </div>
+        `;
+
+        let productInfo = [product.image, product.title, product.description, product.price]
+        localStorage.setItem(product.id, JSON.stringify(productInfo))
+    });
 
 
-output += '</div>'
+    output += '</div>'
 
-document.getElementById('Products').innerHTML = output
+    document.getElementById('Products').innerHTML = output
 }
-  
-  function validateForm() {
-    let name = document.forms["customerForm"]["customerName"].value;
-    if (name == "") {
-      alert("Name must be filled out");
-      return false;
-    }
-    let mail = document.forms["customerForm"]["e-mail"].value;
-    if (mail == "") {
-      alert("Mail must be filled out");
-      return false;
-    }
-    let phone = document.forms["customerForm"]["phoneNumber"].value;
-    if (phone == "") {
-      alert("Phonenumber must be filled out");
-      return false;
-    }
-    let street = document.forms["customerForm"]["street"].value;
-    if (street == "") {
-      alert("Street must be filled out");
-      return false;
-    }
-    let zipcode = document.forms["customerForm"]["zipCode"].value;
-    if (zipcode == "") {
-      alert("Zip-code must be filled out");
-      return false;
-    }
-    let city = document.forms["customerForm"]["city"].value;
-    if (city == "") {
-      alert("City must be filled out");
-      return false;
-    }
-  }
-    
+
---
+++ javascriptOrder.js
@@ -0,0 +1,55 @@
+const params = new URLSearchParams(window.location.search)
+    const id = params.get('id')
+    console.log(id)
+
+    const product = JSON.parse(localStorage.getItem(id))
+    console.log(product)
+
+    document.getElementById('itemToOrder').innerHTML =
+      `<div class="row g-0">
+          <div class="col-md-4">
+            <img src="${product[0]}" class="img-fluid rounded-start" alt="${product[1]}">
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">${product[1]}</h5>
+              <p class="card-text">${product[2]}</p>
+              <p class="card-text"><small class="text-body-secondary">${product[3]}$</small></p>
+            </div>
+          </div>
+        </div>
+    `;
+
+function validateForm() {
+    let name = document.forms["customerForm"]["customerName"].value;
+    if (name == "") {
+      alert("Name must be filled out");
+      return false;
+    }
+    let mail = document.forms["customerForm"]["e-mail"].value;
+    if (mail == "") {
+      alert("Mail must be filled out");
+      return false;
+    }
+    let phone = document.forms["customerForm"]["phoneNumber"].value;
+    if (phone == "") {
+      alert("Phonenumber must be filled out");
+      return false;
+    }
+    let street = document.forms["customerForm"]["street"].value;
+    if (street == "") {
+      alert("Street must be filled out");
+      return false;
+    }
+    let zipcode = document.forms["customerForm"]["zipCode"].value;
+    if (zipcode == "") {
+      alert("Zip-code must be filled out");
+      return false;
+    }
+    let city = document.forms["customerForm"]["city"].value;
+    if (city == "") {
+      alert("City must be filled out");
+      return false;
+    }
+  }
+    
