b001804dfbe850ed98637f3180ff354b36d14c54
PauliLinde <jus.paulina@gmail.com>
2025-03-27 14:00:10 +0100

    Added comments to javascriptOrder

--- javascriptOrder.js
+++ javascriptOrder.js
@@ -1,23 +1,26 @@
+//Hämtar valda produktens id
 const params = new URLSearchParams(window.location.search)
-    const id = params.get('id')
-    console.log(id)
+const id = params.get('id')
+console.log(id)
 
-    const product = JSON.parse(localStorage.getItem(id))
-    console.log(product)
+//Hämtar produktinformation från Local Storage
+const product = JSON.parse(localStorage.getItem(id))
+console.log(product)
 
-    document.getElementById('itemToOrder').innerHTML =
-      `<div class="row g-0">
-          <div class="col-md-4">
+//Lägger till card till formulärsidan
+document.getElementById('itemToOrder').innerHTML =
+    `<div class="row g-0">
+        <div class="col-md-4">
             <img src="${product[0]}" class="img-fluid rounded-start" alt="${product[1]}">
-          </div>
-          <div class="col-md-8">
-            <div class="card-body">
-              <h5 class="card-title">${product[1]}</h5>
-              <p class="card-text">${product[2]}</p>
-              <p class="card-text"><small class="text-body-secondary">${product[3]}$</small></p>
-            </div>
-          </div>
         </div>
+        <div class="col-md-8">
+        <div class="card-body">
+            <h5 class="card-title">${product[1]}</h5>
+            <p class="card-text">${product[2]}</p>
+            <p class="card-text"><small class="text-body-secondary">${product[3]}$</small></p>
+            </div>
+        </div>
+    </div>
     `;
 
 function validateForm() {
