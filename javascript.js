d527ef8a3c7e60a12ad9edad158849409781d558
LEB-Atheel <ado@lebsystem.se>
2025-04-07 11:32:12 +0200

    Changes in JavaScript code

--- javascript.js
+++ javascript.js
@@ -5,18 +5,18 @@ function loadProductPage() {
 }
 
 function render(products) {
-  let output = '<div class="row">';
+  let output = '<div class="row g-4">';
 
   products.forEach((product) => {
     output += `
-<div class="col-12 col-md-6 col-lg-3 mb-4">
-<div class="card" style="width: 18rem;">
+<div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
+<div class="card" style="width: 100%">
   <img src="${product.image}" class="card-img-top" alt="${product.title}">
   <div class="card-body">
     <h5 class="card-title">${product.title}</h5>
-    <a href="order.html?id=${product.id}" class="btn btn-secondary">Buy ${(
+    <a href="order.html?id=${product.id}" class="btn btn-secondary">Köp ${(
       product.price * 10
-    ).toFixed(2)} kr</a>
+    ).toFixed(2)}kr</a>
   </div>
 </div>
 </div>
@@ -49,7 +49,11 @@ function loadProduct() {
   //Lägger till card till formulärsidan
   document.getElementById("itemToOrder").innerHTML = `<div class="row g-0">
         <div class="col-md-4">
-            <img src="${product[0]}" id="image-order" class="img-fluid rounded-start" alt="${product[1]}">
+            <img src="${
+              product[0]
+            }" id="image-order" class="img-fluid rounded-start" alt="${
+    product[1]
+  }">
         </div>
         <div class="col-md-8">
         <div class="card-body">
@@ -57,39 +61,12 @@ function loadProduct() {
             <p class="card-text">${product[2]}</p>
             <p class="card-text"><small class="text-body-secondary">${(
               product[3] * 10
-            ).toFixed(2)} kr</small></p>
+            ).toFixed(2)}kr</small></p>
             </div>
         </div>
     </div>
     `;
 }
-
-function loadCompleteOrder() {
-  console.log(localStorage.getItem("orderId"));
-
-  let orderId = localStorage.getItem("orderId");
-  const product = JSON.parse(localStorage.getItem(orderId));
-
-  document.getElementById("completeOrder").innerHTML = `<div class="row g-0">
-          <div class="col-md-4">
-              <img src="${product[0]}" id="image-order" class="img-fluid rounded-start" alt="${product[1]}">
-          </div>
-          <div class="col-md-8">
-          <div class="card-body">
-              <h5 class="card-title">${product[1]}</h5>
-              
-          <!--Utan info text 
-              <p class="card-text">${product[2]}</p>
-              -->
-              <p class="card-text"><small class="text-body-secondary">${(product[3] * 10).toFixed(2)}kr</small></p>
-              <h4>Order Complete!</h4>
-              <a href="product.html" class="btn btn-secondary">Back to shoping</a>
-              </div>
-          </div>
-      </div>
-      `;
-}
-
 function validateForm(e) {
   e.preventDefault();
   let validater = true;
@@ -101,51 +78,51 @@ function validateForm(e) {
   let zipcode = document.forms["customerForm"]["zipCode"].value;
   let city = document.forms["customerForm"]["city"].value;
 
-  document.getElementById("nameLable").innerHTML = "First- and Last Name:";
+  document.getElementById("nameLable").innerHTML = "För- och Efternamn:";
   if (name.length < 2 || name.length > 50) {
     document.getElementById(
       "nameLable"
-    ).innerHTML += `<div style="color:Tomato">At least 2 symbols, max 50</div>`;
+    ).innerHTML += `<div style="color:Tomato">Minst 2 tecken, max 50</div>`;
     validater = false;
   }
 
-  document.getElementById("mailLable").innerHTML = "E-mail:";
+  document.getElementById("mailLable").innerHTML = "E-post:";
   if (!mail.includes("@") || mail.length > 50) {
     document.getElementById(
       "mailLable"
-    ).innerHTML += `<div style="color:Tomato">Must contain an '@' symbol</div>`;
+    ).innerHTML += `<div style="color:Tomato">En e-post måste innehålla '@'</div>`;
     validater = false;
   }
 
-  document.getElementById("phoneLable").innerHTML = "Phone Number:";
+  document.getElementById("phoneLable").innerHTML = "Telefonnummer:";
   if (/[^\d()-]/.test(phone) || phone == "") {
     document.getElementById(
       "phoneLable"
-    ).innerHTML += `<div style="color:Tomato">Only numbers, hyphen and parentheses</div>`;
+    ).innerHTML += `<div style="color:Tomato">Endast siffror, bindestreck och parenteser</div>`;
     validater = false;
   }
 
-  document.getElementById("streetLable").innerHTML = "Street:";
+  document.getElementById("streetLable").innerHTML = "Gatuadress:";
   if (street.length < 2 || street.length > 50) {
     document.getElementById(
       "streetLable"
-    ).innerHTML += `<div style="color:Tomato">At least 2 symbols, max 50</div>`;
+    ).innerHTML += `<div style="color:Tomato">Minst 2 tecken, max 50</div>`;
     validater = false;
   }
 
-  document.getElementById("zipLable").innerHTML = "Zip code:";
+  document.getElementById("zipLable").innerHTML = "Postnummer:";
   if (zipcode.length != 5 || /[^\d]/.test(zipcode)) {
     document.getElementById(
       "zipLable"
-    ).innerHTML += `<div style="color:Tomato">Exactly 5 numbers</div>`;
+    ).innerHTML += `<div style="color:Tomato">Exakt 5 siffror</div>`;
     validater = false;
   }
 
-  document.getElementById("cityLable").innerHTML = "City:";
+  document.getElementById("cityLable").innerHTML = "Ort:";
   if (city.length < 2 || city.length > 50) {
     document.getElementById(
       "cityLable"
-    ).innerHTML += `<div style="color:Tomato">At least 2 symbols, max 50</div>`;
+    ).innerHTML += `<div style="color:Tomato">Minst 2 tecken, max 50</div>`;
     validater = false;
   }
 
@@ -154,6 +131,33 @@ function validateForm(e) {
     alert("Order Successful");
     window.location = "orderComplete.html";
   }
+  //return validater;
 }
 
+function loadCompleteOrder() {
+  console.log(localStorage.getItem("orderId"));
 
+  let orderId = localStorage.getItem("orderId");
+  const product = JSON.parse(localStorage.getItem(orderId));
+
+  document.getElementById("completeOrder").innerHTML = `<div class="row g-0">
+          <div class="col-md-4">
+              <img src="${
+                product[0]
+              }" id="image-order" class="img-fluid rounded-start" alt="${
+    product[1]
+  }">
+          </div>
+          <div class="col-md-8">
+          <div class="card-body">
+              <h5 class="card-title">${product[1]}</h5>
+              <p class="card-text">${product[2]}</p>
+              <p class="card-text"><small class="text-body-secondary">${(
+                product[3] * 10
+              ).toFixed(2)}kr</small></p>
+              </div>
+          </div>
+      </div>
+      `;
+  //localStorage.removeItem("orderId");
+}
