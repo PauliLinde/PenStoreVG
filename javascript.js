84fb70a389e8af681683db513f289e0e7ac8195d
PauliLinde <jus.paulina@gmail.com>
2025-04-09 08:35:35 +0200

    First commit, added shopping card

--- javascript.js
+++ javascript.js
@@ -1,3 +1,7 @@
+/*let shoppingCart = [];
+
+localStorage.setItem('shoppingCart', JSON.stringify(shoppingCart))*/
+
 function loadProductPage() {
   fetch("https://fakestoreapi.com/products")
     .then((res) => res.json())
@@ -7,6 +11,7 @@ function loadProductPage() {
 function render(products) {
   let output = '<div class="row g-4">';
 
+
   products.forEach((product) => {
     output += `
 <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
@@ -14,7 +19,7 @@ function render(products) {
   <img src="${product.image}" class="card-img-top" alt="${product.title}">
   <div class="card-body">
     <h5 class="card-title">${product.title}</h5>
-    <a href="order.html?id=${product.id}" class="btn btn-secondary">Buy ${(
+    <a href="order.html?id=${product.id}" class="btn btn-secondary" onclick="addToCart(${product.id})">Buy ${(
       product.price * 10
     ).toFixed(2)} kr</a>
   </div>
@@ -25,10 +30,12 @@ function render(products) {
       product.image,
       product.title,
       product.description,
-      product.price,
+      (product.price *10).toFixed(2),
+      0
     ];
     localStorage.setItem(product.id, JSON.stringify(productInfo));
   });
+
 
   output += "</div>";
 
@@ -55,9 +62,7 @@ function loadProduct() {
         <div class="card-body">
             <h5 class="card-title">${product[1]}</h5>
             <p class="card-text">${product[2]}</p>
-            <p class="card-text"><small class="text-body-secondary">${(
-              product[3] * 10
-            ).toFixed(2)} kr</small></p>
+            <p class="card-text"><small class="text-body-secondary">${product[3]} kr</small></p>
             </div>
         </div>
     </div>
@@ -81,13 +86,59 @@ function loadCompleteOrder() {
           <!--Utan info text 
               <p class="card-text">${product[2]}</p>
               -->
-              <p class="card-text"><small class="text-body-secondary">${(product[3] * 10).toFixed(2)} kr</small></p>
+              <p class="card-text"><small class="text-body-secondary">${product[3]} kr</small></p>
               <h4>Order Complete!</h4>
               <a href="product.html" class="btn btn-secondary">Back to shoping</a>
               </div>
           </div>
       </div>
       `;
+}
+
+function addToCart(produktID){
+  let product = JSON.parse(localStorage.getItem(produktID));
+  let shoppingCart = JSON.parse(localStorage.getItem('shoppingCart')) || []
+  let existing = shoppingCart.find(item => item [0] === product[0])
+
+  if(existing){
+    existing[4] += 1
+  }
+  else{
+    product[4] = 1
+    shoppingCart.push(product)
+  }
+  localStorage.setItem('shoppingCart', JSON.stringify(shoppingCart));
+}
+
+function  getCart(){
+  let shoppingCart = JSON.parse(localStorage.getItem('shoppingCart'))
+  let output = '';
+
+  if(shoppingCart) {
+    console.log("Det finns varor")
+    
+    shoppingCart.forEach((product) => {
+      output += `<div class="row g-0">
+          <div class="col-md-4">
+              <img src="${product[0]}" id="image-order" class="img-fluid rounded-start" alt="${product[1]}">
+          </div>
+          <div class="col-md-8">
+          <div class="card-body">
+              <h5 class="card-title">${product[1]}</h5>
+              <p class="card-text"><small class="text-body-secondary">${
+                product[4]}</small></p>
+              </div>
+          </div>
+      </div>
+      `;
+  });
+  }
+  else{
+    console.log("Korgen är tom")
+    output += `<h4>Your cart is empty</h4>`
+  }
+
+  document.getElementById("Cart").innerHTML = output;
 }
 
 function validateForm(e) {
